b44e6f223e0610c770446aa118a7c350cf8f4b9b
jinyu71Y18106 <1144276183@qq.com>
2021-07-12 09:13:04 +0800

    更新了注册登录代码

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('Asia/Shanghai');
 header('Content-Type: application/json; charset=utf-8'); //json
 //header("Content-type: text/html; charset=utf-8");
 header('Access-Control-Allow-Origin:*');
@@ -8,41 +9,63 @@ $ro =file_get_contents('php://input');
 $user=json_decode($ro,true);
 $myusername=$user['username'];
 $mypassword=$user['password'];
-//$myusername = $_GET[username];
-//$mypassword = md5($_GET[password]);
-if($myusername!=''&&$mypassword!=''){
+
+
+function preg_username($username){
+    if(preg_match("/^[a-zA-Z][a-zA-Z0-9_]{3,19}/",$username)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function checkpassword($password){
+    $length = strlen($password);
+    if($length>=6&&$length<=27){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+if(preg_username($myusername)&&checkpassword($mypassword)){
 
     $con = mysqli_connect("localhost","root","","users");
 
     if($con) {
+        $mypassword = md5($mypassword);
         $select= mysqli_select_db($con,"users");
         $sql="SELECT * FROM `users` WHERE `users---username` = '$myusername' AND `users---password` = '$mypassword'";
         $result=mysqli_query($con,$sql);
         $re = mysqli_num_rows($result);
         mysqli_free_result($result);
 
-
-
-//        $sql = "select users---username,users---password FROM users where users---username='$myusername' and users---password = '$mypassword' ";
-//        $result = $con->query($sql);
-//        $resArray = mysqli_fecth_array($result);
         if ($re!=0) {
             $row['status']="1";
             $row['err']="0";
+//            $last = mysqli_query($con, "SELECT `users---last_login_at` FROM `users` ");
+//            $la = mysqli_fetch_array($last);
+            $last_login = date("Y-m-d H:i:s");
+
+            mysqli_query($con,"UPDATE `users` SET `users---last_login_at`='$last_login' WHERE `users---username`='$myusername' ");
+
+
 
         } else {
             $row['status']="0";
             $row['err']="fail";
-            echo "用户名或密码错误";
+
 
         }
         echo(json_encode($row));
         mysqli_close($con);
     }
+}else{
+    $row['status'] = '2' ;
+    $row['err'] = 'fail';
+    echo(json_encode($row));//后台数据校验不通过
 }
-//    else{
-//        echo "无法连接数据库";
-//    }
+
 
 
 
--- register.php
+++ register.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('Asia/Shanghai');
 header("Content-Type: text/html;charset=utf-8");
 header('Access-Control-Allow-Origin:*');
 $ro =file_get_contents('php://input');
@@ -13,128 +14,117 @@ $myprovince=$user['province'];
 $mycity=$user['city'];
 $myarea=$user['area'];
 $address=$myprovince.$mycity.$myarea;
-$address1="江苏江苏江苏";
-$con = mysqli_connect("localhost","root","","users");
+$birth = str_replace("T16:00:00.000Z","",$mybirthday);////转换传入后端的日期格式,使其符合数据库字段类型
+$birth = date("Y-m-d",strtotime("+1 day",strtotime($birth)));//日期+1，因为之前的日期格式直接转换成字符串会少一天
+
 //检查输入是否符合规范
-//checkName() {
-//    //焦点消失 判断用户名输入值
-//      var regula = new RegExp("^[a-zA-Z][a-zA-Z0-9_]{3,19}");
-//      if (regula.test(this.inputName)) {
-//          this.messageName = '';
-//          //合法
-//      } else {
-//          //不合法
-//          console.log('不合法');
-//          //不合法红色警告符
-//          this.messageName =
-//              '!请输入4-20字符，首字母为英文，由字母、数字、下划线组成';
-//      }
-//    },
-//
-//    checkNickname() {
-//      var nick = this.inputNick;
-//      var name = this.inputName;
-//      if (nick === '') {
-//          nick = name;
-//          this.inputNick = this.inputName;
-//      }
-//      if (nick.length < 4) {
-//          console.log('不合法');
-//          this.messageNick = '昵称太短了';
-//      } else {
-//          this.messageNick = '';
-//      }
-//    },
-//
-//    checkKey() {
-//      var key = this.inputKey;
-//      var confirm = this.inputConfirm;
-//      if (key === '') {
-//          console.log('不合法');
-//          this.messageKey = '!请输入密码';
-//      } else {
-//          this.messageKey = '';
-//      }
-//      if (key.length < 6) {
-//          console.log('不合法');
-//          this.messageKey = '!密码太短了';
-//      } else {
-//          this.messageKey = '';
-//      }
-//
-//      if (confirm != key) {
-//          console.log('不合法');
-//          this.messageConfirm = '!两次密码不一致';
-//      } else {
-//          this.messageConfirm = '';
-//      }
-//    },
-//
-//    checkConfirm() {
-//      var key = this.inputKey;
-//      var confirm = this.inputConfirm;
-//      if (confirm != key) {
-//          console.log('不合法');
-//          this.messageConfirm = '!两次密码不一致';
-//      } else {
-//          this.messageConfirm = '';
-//      }
-//    },
-//
-//    checkProcince(a) {
-//    console.log(a)
-//      this.province=a.value
-//      this.messageprovince='1'
-//
-//    },
-//
-//     checkCity(a) {
-//     console.log(a)
-//      this.city=a.value
-//      this.messagecity='1'
-//    },
-//
-//     checkArea(a) {
-//     console.log(a)
-//      this.area=a.value
-//      this.messagearea='1'
-//    },
-//
-//   checkMail() {
-//      var regula = new RegExp(
-//    "^[a-z0-9A-Z]+[- | a-z0-9A-Z . _]+@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-z]{2,}$"
-//);
-//      if (regula.test(this.inputMail)) {
-//          //合法
-//          this.messageMail='';
-//      } else {
-//          console.log('不合法');
-//          this.messageMail = '请输入正确的邮箱格式';
-//          return false;
-//      }
-//    },
 
-if($con) {
-    mysqli_select_db($con, "users");
-    $sql1 = "SELECT * FROM `users` WHERE `users---username` = '$myusername'";
-    $check = mysqli_query($con, $sql1);
-    $ch = mysqli_num_rows(check);
-    mysqli_free_result($check);
-    if ($ch != 0) {
-        $row['status'] = "0";
-        $row['err'] = "fail";
-    } else {
-        $result = mysqli_query($con, "SELECT `users---id` FROM `users` WHERE `users---id` = (select max(`users---id`)from `users`)");
-        $re = mysqli_fetch_array($result);
-        $userid = $re['users---id'] + 1;
-
-        $sql = "INSERT INTO `users` (`users---id`, `users---username`, `users---password`, `users---nickname`, `users---head`, `users---email`, `users---birthday`, `users---sex`, `user--address`, `users---last_login_at`, `users---updated_at`, `users---created_at`) 
-VALUES ('$userid', '$myusername', '$mypassword', '$mynickname', NULL, '$myemail', NULL , '$mysex','$address1', NULL, NULL, NULL);";
-        mysqli_query($con, $sql);
-        $row['status']="0";
-        $row['err'] = "0";
+//验证用户名
+function preg_username($username){
+    if(preg_match("/^[a-zA-Z][a-zA-Z0-9_]{3,19}/",$username)){
+        return true;
+    }else{
+        return false;
     }
-    echo(json_encode($row));
-    mysqli_close($con);
+}
 
+
+//验证昵称
+function checknicknam($nickname){
+    $length = strlen($nickname);
+    if($nickname!=''&&$length>=4&&$length<=20){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+//验证密码
+function checkpassword($password){
+    $length = strlen($password);
+    if($length>=6&&$length<=27){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+//验证性别
+function checksex($sex){
+    if($sex!=''){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+//验证生日
+function checkbirth($birth){
+    if($birth!=''){
+        return true;
+    }else{
+        return  false;
+    }
+}
+
+//验证所在地
+function checklocation($location){
+    if($location!=''){
+        return true;
+    }else{
+        return  false;
+    }
+}
+
+//验证邮箱
+function  preg_email($mail){
+    if(preg_match("/^[a-z0-9A-Z]+[- | a-z0-9A-Z . _]+@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-z]{2,}$/",$mail)){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+if(preg_username($myusername)&&checknicknam($mynickname)&&checkpassword($mypassword)&&checkbirth($mybirthday)&&checklocation($address)&&preg_email($myemail)) {
+    $created=date("Y-m-d H:i:s");
+
+    $con = mysqli_connect("localhost", "root", "", "users");
+    if ($con) {
+        $mypassword = md5($mypassword);
+        mysqli_select_db($con, "users");
+        $sql1 = "SELECT * FROM `users` WHERE `users---username` = '$myusername'";//检查用户名是否已经注册
+        $check = mysqli_query($con, $sql1);//执行mysql语句
+        $ch = mysqli_num_rows($check);//返回结果集的函数，若存在则返回1（一行结果集）
+        mysqli_free_result($check);//释放查询结果内存
+        ////判断查询结果集，将结果返回给前端
+        if ($ch != 0) {
+            //数据库中查询到注册时填写的用户名，注册失败
+            $row['status'] = "0";
+            $row['err'] = "fail";
+        } else {
+            //数据库中没有重复用户名
+            $result = mysqli_query($con, "SELECT `users---id` FROM `users` WHERE `users---id` = (select max(`users---id`)from `users`)");
+            $re = mysqli_fetch_array($result);
+            $userid = $re['users---id'] + 1;
+
+            $sql = "INSERT INTO `users` (`users---id`, `users---username`, `users---password`, `users---nickname`, `users---head`, `users---email`, `users---birthday`, `users---sex`, `user--address`, `users---last_login_at`, `users---updated_at`, `users---created_at`) 
+VALUES ('$userid', '$myusername', '$mypassword', '$mynickname', NULL , '$myemail', '$birth', '$mysex','$address', NULL, NULL, '$created');";
+            mysqli_query($con, $sql);
+            $row['status'] = "1";
+            $row['err'] = "0";
+        }
+        echo(json_encode($row));
+        mysqli_close($con);//关闭数据库
+
+    }else{
+        $row['status']='3';
+        $row['err']='false';
+        echo(json_encode($row));//数据库未连接
+
+    }
+}else{
+    $row['status'] = '2' ;
+    $row['err'] = 'fail';
+    echo(json_encode($row));//后台数据校验不通过
 }
